2c4b992abe14327168f2f3b08bebf3a998429612
WilliamKasurak <willkdesign@gmail.com>
2018-08-06 17:04:06 +0000

    newline added

--- HTTPReq/http.js
+++ HTTPReq/http.js
@@ -1,31 +1,19 @@
 var https  = require('https');
 
-// var request = {
-//   host: 'www.juliamarcello.com',
-//   path: '/we-love-you-goodnight#5'
-// };
-
-
-function getAndPrintHTMLChunks () {
-
-  var requestOptions = {
+var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step1.html'
   };
+function getAndPrintHTMLChunks () {
+
   /* Add your code here */
   https.get(requestOptions, function (response) {
-
     // set encoding of received data to UTF-8
     response.setEncoding('utf8');
-
     // the callback is invoked when a `data` chunk is received
     response.on('data', function (data) {
-     console.log('Chunk Received.', data);
+     console.log('\n' , data);
      });
-
-    response.on('end', function() {
-      console.log('Response stream complete.');
-    });
   });
 }
 getAndPrintHTMLChunks();
